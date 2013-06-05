f369775ef7c299bce4f713f4b3902ececc3a04c0
fhemery <fhemery@hemit.fr>
2013-06-05 12:18:49 +0200

    First simple robot
    Implemented a robot that aims with 100% accuracy at a still robot.

---
+++ TestRobots/src/simpleBots/ElephantInCorridorsSniper.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleBots;
+
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+/**
+ *
+ * @author FHEMERY
+ */
+public class ElephantInCorridorsSniper extends AdvancedRobot {
+
+    @Override
+    public void run() {
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        while (true) {
+            ahead(100);
+            turnRadarRight(360);
+            back(100);
+            turnRadarRight(360);
+        }
+    }
+    
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Turn the gun so as to get it right.
+        double gunAngle = getGunHeading() - getHeading();
+        double relativeEnemyAngle = e.getBearing();
+        System.out.println("Gun angle is:"+gunAngle);
+        System.out.println("bearing is: "+relativeEnemyAngle);
+          
+        
+        turnGunRight(relativeEnemyAngle - gunAngle);
+        fire(1);
+    }
+}
